c0d8023e2bdb000c238c618e5fc6ee47503aeb02
Ahmed Mostafa <ahmed-mostafa2020>
2024-02-29 00:32:43 +0200

    w o toolkit3

--- src/store/index.js
+++ src/store/index.js
@@ -2,22 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./counterSlice";
 import authReducer from "./authSlice";
 
-// const counterReducer = (state = initState, action) => {
-//   ///logic
-//   if (action.type === "increase") {
-//     return { ...state, value: state.value + action.payload };
-//   }
-
-//   if (action.type === "decrease") {
-//     return { ...state, value: state.value - action.payload };
-//   }
-
-//   if (action.type === "toggleCounter") {
-//     return { ...state, showCounter: !state.showCounter };
-//   }
-//   return state;
-// };
-
 const store = configureStore({
   reducer: {
     counter: counterReducer,
